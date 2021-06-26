b85224d6e296bb1253cecae373f845ce3ff6873b
StoneMooseGames <mika.ahlsten@stonemoose.com>
2021-06-26 19:09:02 +0300

    Added a lot of stuff.
    First enemy animations are set. Controls for player are ready

---
+++ Assets/BulletBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletBehaviour : MonoBehaviour
+{
+    private float bulletSpeed;
+    public GameObject hitFX;
+    public GameObject gunShotFX;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        bulletSpeed = GameObject.Find("Player").GetComponent<PlayerCharacterController>().bulletSpeed;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * bulletSpeed * Time.deltaTime);
+        Destroy(this.gameObject, 5.0f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject instantiatedObj = (GameObject)Instantiate(hitFX, this.gameObject.transform.position, this.gameObject.transform.rotation);
+        Destroy(instantiatedObj, 1.0f);
+
+        Destroy(this.gameObject);
+
+       
+    }
+
+}
---
+++ Assets/PlayerCharacterController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+   
+    
+    public float bulletSpeed = 10;
+    public Rigidbody bullet;
+    private Rigidbody bulletClone;
+    private GameObject weapon1;
+    private GameObject weapon2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon1 = GameObject.FindGameObjectWithTag("Weapon1");
+        weapon2 = GameObject.FindGameObjectWithTag("Weapon2");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FireWeapon();
+    }
+
+    
+
+    private void FireWeapon()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            bulletClone = Instantiate(bullet,weapon2.transform.position, weapon2.transform.rotation);
+            
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            bulletClone = Instantiate(bullet, weapon1.transform.position, weapon1.transform.rotation);
+
+        }
+    }
+}
